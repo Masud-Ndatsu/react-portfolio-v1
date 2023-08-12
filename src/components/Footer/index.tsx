f09f8b2cf9d843b47e4a1f98679a4344be63c471
Masud-Ndatsu <masudndatsu@gmail.com>
2023-08-12 18:08:23 +0100

    feat: added count-api stats

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+import countapi from "countapi-js";
+
 export const Footer = () => {
+  const [viewCount, setViewCount] = useState(0);
+
+  useEffect(() => {
+    countapi
+      .visits(
+        "https://api.countapi.xyz/hit/https://masud-biggie.vercel.app/visits"
+      )
+      .then((result) => {
+        console.log(result);
+        setViewCount(result.value);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <footer className="text-white p-4 py-8 max-w-[1200px] m-auto">
       <>
@@ -6,7 +23,7 @@ export const Footer = () => {
           <h1 className="text-xl">
             &#169; {new Date().getFullYear()} Masud Ndatsu
           </h1>
-          <p>Site stats - views</p>
+          <p>Site stats - {viewCount ?? 0} views</p>
         </div>
       </>
     </footer>
